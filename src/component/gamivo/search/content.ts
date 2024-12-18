15af62a6603c742bbb52ced471477912b487a0ce
ko1steve <ko1steveg@gmail.com>
2024-12-18 11:38:06 +0800

    fix header

--- src/component/gamivo/search/content.ts
+++ src/component/gamivo/search/content.ts
@@ -11,8 +11,11 @@ class GamivoSearchController extends ComponentController {
     if (!header) {
       return null;
     }
-    header.className += ' flexbox';
     return header;
+  }
+
+  protected modifyHeader (header: HTMLElement): void {
+    header.className += ' flexbox';
   }
 
   protected isGameListFirstChildExist (children: HTMLElement[]): boolean {
