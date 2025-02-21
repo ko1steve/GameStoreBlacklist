fa1de253259dd959b489ab94d73f0f7d4edda1aa
ko1steve <ko1steveg@gmail.com>
2025-02-21 16:37:09 +0800

    update support ggdeals

--- config/webpack.common.ts
+++ config/webpack.common.ts
@@ -24,6 +24,7 @@ module.exports = {
     'src/component/web/gamivo/search/content': './src/component/web/gamivo/search/content.ts',
     'src/component/web/ggdeals/product/content': './src/component/web/ggdeals/product/content.ts',
     'src/component/web/ggdeals/deals/content': './src/component/web/ggdeals/deals/content.ts',
+    'src/component/web/ggdeals/games/content': './src/component/web/ggdeals/games/content.ts',
     'src/component/web/yuplay/product/content': './src/component/web/yuplay/product/content.ts',
     'src/component/web/yuplay/search/content': './src/component/web/yuplay/search/content.ts',
     'src/component/web/humbleBundle/mainPage/content': './src/component/web/humbleBundle/mainPage/content.ts',
