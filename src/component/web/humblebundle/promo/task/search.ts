838c09c40eb59ba72687368c5aa62bdc7fd1f431
ko1steve <ko1steveg@gmail.com>
2025-02-21 15:49:03 +0800

    remove unused

--- src/component/web/humblebundle/promo/task/search.ts
+++ src/component/web/humblebundle/promo/task/search.ts
@@ -5,41 +5,8 @@ export class HumbleBundleSearchTaskHandler extends ListTaskHandler {
     return document.getElementsByClassName('chunks-container')[0].getElementsByClassName('entities-list')[0] as HTMLDivElement;
   }
 
-  // protected modifyGameInfoElement (infoElement: HTMLElement): HTMLElement | null {
-  //   const ahrefParent = infoElement.children[0]?.children[0] as HTMLElement;
-  //   const ahrefElement = ahrefParent?.children[0];
-  //   const ahrefElementChild = ahrefElement?.children[0];
-  //   if (ahrefElementChild) {
-  //     if (!ahrefParent.dataset.hasInit) {
-  //       ahrefElement.removeChild(ahrefElementChild);
-  //       ahrefParent.removeChild(ahrefElement);
-  //       const container = document.createElement('div');
-  //       container.className = ahrefElement.className;
-  //       container.appendChild(ahrefElementChild);
-  //       ahrefParent.insertBefore(container, ahrefParent.firstChild);
-  //       ahrefParent.dataset.hasInit = 'true';
-  //     }
-  //     const titleElementParent = infoElement.children[0]?.children[0]?.children[0]?.children[0]?.children[1];
-  //     const titleElement = titleElementParent?.children[0] as HTMLElement;
-  //     if (titleElement) {
-  //       if (!titleElement.dataset.hasInit) {
-  //         titleElementParent.removeChild(titleElement);
-  //         titleElementParent.appendChild(ahrefElement);
-  //         ahrefElement.appendChild(titleElement);
-  //         titleElement.dataset.hasInit = 'true';
-  //       }
-  //     } else {
-  //       return null;
-  //     }
-  //   } else {
-  //     return null;
-  //   }
-  //   return infoElement;
-  // }
-
   protected getCheckboxParent (infoContainer: HTMLElement): HTMLElement | null {
     return infoContainer;
-    // return infoContainer.getElementsByClassName('entity-details')[0] as HTMLElement;
   }
 
   protected getRawGameTitle (infoContainer: HTMLElement): string | undefined {
