b904094a8c9d998c74e3451e332099cb35dd0ea6
ko1steve <ko1steveg@gmail.com>
2025-02-24 14:24:31 +0800

    update ggdeals

--- src/component/web/ggdeals/product/task/product.ts
+++ src/component/web/ggdeals/product/task/product.ts
@@ -2,23 +2,11 @@ import { ProductTaskHandler } from './../../../../../core/task/productTaskHandle
 
 export class GgdealsProductTaskHandler extends ProductTaskHandler {
   protected getCheckboxParent (): HTMLElement | undefined {
-    const gameCardElement = document.getElementById('game-card');
-    const gameHeaderElement = gameCardElement?.getElementsByClassName('game-header')[0];
-    const gameSectionElement = gameHeaderElement?.getElementsByClassName('game-section')[0];
-    const colLeftElement = gameSectionElement?.getElementsByClassName('col-left')[0];
-    const gameHeaderBox = colLeftElement?.getElementsByClassName('game-header-box')[0];
-    const wrapperElement = gameHeaderBox?.getElementsByClassName('game-info-image-wrapper')[0] as HTMLElement;
-    if (!wrapperElement) {
-      return undefined;
-    }
-    wrapperElement.style.position = 'relative';
-    return wrapperElement;
+    return document.getElementsByClassName('game-heading')[0] as HTMLElement;
   }
 
   protected getRawGameTitle (): string | undefined {
-    const catagoryListElement = document.getElementById('page')
-      ?.getElementsByClassName('relative breadcrumbs-widget pjax-inner-replace')[0]?.children[0]?.children[0];
-    const length = catagoryListElement?.children.length!;
-    return (catagoryListElement?.children[length - 1]?.children[0]?.children[0] as HTMLElement)?.innerText;
+    const catagoryListElement = document.getElementById('page')?.getElementsByClassName('breadcrumbs-list')[0];
+    return catagoryListElement?.lastElementChild?.getElementsByTagName('span')[0]?.innerText;
   }
 }
