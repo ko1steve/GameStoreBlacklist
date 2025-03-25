46d13aef06f100c264059e6e6708ee61e1383491
ko1steve <ko1steveg@gmail.com>
2025-03-25 15:38:28 +0800

    fix main page

--- src/component/web/greenmangaming/config/main-page.ts
+++ src/component/web/greenmangaming/config/main-page.ts
@@ -1,13 +1,21 @@
 import { TaskHandler } from '../../../../core/task/task-handler';
 import { DataModel } from '../../../../model/data-model';
-import { GreenManGamingLargeProductBlockExcludeFirstTaskHandler } from '../task/large-product-block-exclude-first';
 import { GreenManGamingMediumProductBlockModuleContentTaskHandler } from '../task/medium-product-block-module-content';
-import { ComponentConfig } from './../../../../core/component/component-config';
+import { ComponentConfig, ITextHandleConfig } from './../../../../core/component/component-config';
 
 export class GreenManGamingMainPageConfig extends ComponentConfig {
   public componentId: string = 'greenmangaming-main-page';
+  public texthandle: ITextHandleConfig = {
+    startWords: [],
+    endWords: ['- PC'],
+    cutToEndWords: [],
+    excludeTitleWords: [],
+    excludeClassNames: [
+      '/publisher-sale/', '/spring-sale/'
+    ]
+  };
+
   public taskClasses: (new (componentConfig: ComponentConfig, dataModel: DataModel) => TaskHandler)[] = [
-    GreenManGamingLargeProductBlockExcludeFirstTaskHandler,
     GreenManGamingMediumProductBlockModuleContentTaskHandler
   ];
 }
