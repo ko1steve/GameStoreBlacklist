1e55f62929aab1289f6845c77d18cd70ae502fe4
ko1steve <ko1steveg@gmail.com>
2025-03-27 15:26:11 +0800

    support 2game's search page

--- src/component/web/2game/config.ts
+++ src/component/web/2game/config.ts
@@ -2,21 +2,38 @@ import { TSMap } from 'typescript-map';
 import { ComponentConfig } from './../../../core/component/component-config';
 import { WebObserverConfig } from '../../../core/web-observer/web-observer-config';
 import { TwoGameMainPageConfig } from './config/main-page';
+import { TwoGameSearchConfig } from './config/search';
 
 export enum ControllerType {
-  MAIN_PAGE = 'MAIN_PAGE'
+  MAIN_PAGE = 'MAIN_PAGE',
+  SEARCH = 'SEARCH'
 }
 
 export class TwoGameWebObserverConfig extends WebObserverConfig {
   public urlTypeMap = new TSMap<RegExp, ControllerType>([
     [
       /^https:\/\/2game\.com\/$/, ControllerType.MAIN_PAGE
+    ],
+    [
+      /^https:\/\/2game\.com\/((en|zh)\/)?bestsellers$/, ControllerType.SEARCH
+    ],
+    [
+      /^https:\/\/2game\.com\/((en|zh)\/)?coming-soon$/, ControllerType.SEARCH
+    ],
+    [
+      /^https:\/\/2game\.com\/((en|zh)\/)?new-in$/, ControllerType.SEARCH
+    ],
+    [
+      /^https:\/\/2game\.com\/((en|zh)\/)?search\/.+$/, ControllerType.SEARCH
     ]
   ]);
 
   public configClassMap = new TSMap<string, new() => ComponentConfig>([
     [
       ControllerType.MAIN_PAGE, TwoGameMainPageConfig
+    ],
+    [
+      ControllerType.SEARCH, TwoGameSearchConfig
     ]
   ]);
 }
